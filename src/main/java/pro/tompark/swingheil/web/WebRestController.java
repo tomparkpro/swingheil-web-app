a922a55501f1872081448db27d4a085dbb9e3e58
tomparkpro <tom.hyunung.park@gmail.com>
2018-05-04 18:55:02 +0900

    integrate swingheil-service project

--- src/main/java/pro/tompark/swingheil/web/WebRestController.java
+++ src/main/java/pro/tompark/swingheil/web/WebRestController.java
@@ -1,13 +1,28 @@
 package pro.tompark.swingheil.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.tompark.swingheil.model.Event;
+import pro.tompark.swingheil.service.EventService;
+
+import java.util.List;
 
 @RestController
 public class WebRestController {
 
+    @Autowired
+    EventService eventService;
+
     @GetMapping("/hello")
     public String hello() {
+        return "HelloWorld";
+    }
+
+    @GetMapping("/events")
+    public String events() {
+        List<Event> events = eventService.getEvents();
+        System.out.println(events.size());
         return "HelloWorld";
     }
 }
